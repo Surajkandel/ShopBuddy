26a6462eeda1f1cdb6962d752a5d1463dd26ca18
Surajkandel <107171189+Surajkandel@users.noreply.github.com>
2025-05-05 20:35:23 +0545

    Project initilize

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import './index.css'
+
+function App() {
+  return (
+  <div >
+<h1 className="text-5xl font-bold text-red-500">Tailwind Works!</h1>
+
+
+  </div>
+  
+  );
+}
+
+export default App;
