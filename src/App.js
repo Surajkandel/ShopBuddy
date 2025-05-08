8ee525a232f249ca56b858a5d298742ee0128791
Surajkandel <107171189+Surajkandel@users.noreply.github.com>
2025-05-08 13:23:53 +0545

    commit

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@
 import './App.css';
 import './index.css'
 import { Outlet } from 'react-router-dom';
+import Header from './components/Header'
 
 function App() {
   return (
   <>
+  <Header/>
     <Outlet/>
   </>
   
